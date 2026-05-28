b4508727f9a7780f6db98ae70f4bde6b3b181f33
b-zhengji.a.wang <b-zhengji.a.wang@rakuten.com>
2026-05-28 15:12:08 +0900

    Added Error Chapter

--- errors/exercise12.cpp
+++ errors/exercise12.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
+#include <random>
 #include <stdexcept>
 #include <vector>
 
-void score(std::vector<int> secret, std::vector<int> guess) {
+struct Score {
   int bulls = 0;
   int cows = 0;
+};
+
+Score score(const std::vector<int> &secret, const std::vector<int> &guess) {
+  if (secret.size() != guess.size()) {
+    throw std::runtime_error("secret and guess have different sizes");
+  }
+
+  Score result;
 
   std::vector<bool> secret_used(secret.size(), false);
   std::vector<bool> guess_used(guess.size(), false);
-  std::vector<int> used;
-  if (secret.size() != guess.size())
-    throw std::runtime_error("there is something wrong with this game.");
+
+  // First pass: bulls
   for (std::size_t i = 0; i < secret.size(); ++i) {
     if (secret.at(i) == guess.at(i)) {
-      ++bulls;
+      ++result.bulls;
       secret_used.at(i) = true;
       guess_used.at(i) = true;
     }
   }
 
+  // Second pass: cows
   for (std::size_t i = 0; i < guess.size(); ++i) {
-    if (guess_used.at(i))
+    if (guess_used.at(i)) {
       continue;
+    }
 
     for (std::size_t j = 0; j < secret.size(); ++j) {
-      if (secret_used.at(j))
+      if (secret_used.at(j)) {
         continue;
+      }
 
       if (guess.at(i) == secret.at(j)) {
-        ++cows;
+        ++result.cows;
         guess_used.at(i) = true;
         secret_used.at(j) = true;
         break;
@@ -36,5 +47,53 @@ void score(std::vector<int> secret, std::vector<int> guess) {
     }
   }
 
-  std::cout << "bull is " << bulls << " and cow is " << cows << "\n";
+  return result;
+}
+
+std::vector<int> make_secret() {
+  std::random_device rd;
+  std::default_random_engine engine(rd());
+  std::uniform_int_distribution<int> dist(0, 9);
+
+  std::vector<int> secret;
+
+  for (int i = 0; i < 4; ++i) {
+    secret.push_back(dist(engine));
+  }
+
+  return secret;
+}
+
+int main() {
+  try {
+    std::vector<int> secret = make_secret();
+
+    while (true) {
+      int x1;
+      int x2;
+      int x3;
+      int x4;
+
+      std::cout << "Please enter your guess: ";
+
+      if (!(std::cin >> x1 >> x2 >> x3 >> x4)) {
+        throw std::runtime_error("bad input");
+      }
+
+      std::vector<int> guess = {x1, x2, x3, x4};
+
+      Score result = score(secret, guess);
+
+      std::cout << result.bulls << " bull(s) and " << result.cows
+                << " cow(s)\n";
+
+      if (result.bulls == 4) {
+        std::cout << "You win!\n";
+        break;
+      }
+    }
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
+  }
 }
